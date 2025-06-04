fc2fa752276ab64044c5ff50939e6bf5c5434705
Magnus Krogh <magnus.n.krogh@gmail.com>
2025-06-04 12:05:57 +0200

    data analysis

--- Legeplads.R
+++ Legeplads.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(tidymodels)
 
 data <- titanic_train %>% 
   drop_na() %>%
@@ -6,12 +7,41 @@ data <- titanic_train %>%
   mutate(Pclass = as.factor(Pclass)) %>%
   mutate(Sex = as.factor(Sex)) %>%
   mutate(Embarked = as.factor(Embarked)) %>%
-  mutate(Survived = as.factor(Survived))
+  mutate(Survived = as.factor(Survived)) %>%
+  mutate(LogFare = log(Fare))
 View(data)
 
 data %>%
-  ggplot(aes(Age, log(Fare), group = Survived, color = Survived)) +
+  ggplot(aes(Age, LogFare, group = Survived, color = Survived)) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = lm, se = FALSE, aes(color = Survived)) +
   theme_minimal()
 
+lr_mod <- 
+  logistic_reg() %>%
+  set_engine("glm")
+
+rec <- recipe(Survived ~ Pclass + Sex + Age + LogFare + Embarked, data = data)
+
+wflow <-
+  workflow() %>%
+  add_model(lr_mod) %>%
+  add_recipe(rec)
+wflow
+
+data_split <- initial_split(data, prop = 3/4)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+
+fit <- wflow %>% fit(data = train_data)
+fit %>% extract_fit_parsnip() %>% tidy()
+titanic_aug <- augment(fit, test_data)
+titanic_aug %>% roc_curve(truth = Survived, .pred_0) %>% autoplot() 
+titanic_aug %>% roc_auc(truth = Survived, .pred_0)
+
+test <- randomForest(Survived ~ Pclass + Sex + Age + LogFare + Embarked, data = train_data, ntree = 5000)
+predictions <- predict(test, newdata = test_data, type = "prob")[, 2]
+rf.roc <- roc(test_data$Survived, predictions, levels = rev(levels(test_data$Survived)))
+auc(rf.roc)
+plot.roc(rf.roc)
